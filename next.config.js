df63ab5544dafc4ae173a676387539b05b84ce2b
daveg7lee <daveg7lee@gmail.com>
2021-04-09 18:15:56 +0900

    Change PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
+  // other next config
 });
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,13 +10,34 @@ class MyDocument extends Document {
             content="iVIdX2Zr88cCNMJzRYM-wmA9vVXbKako6kSJigxu2Og"
           />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="description" content="Description" />
-          <meta name="keywords" content="Keywords" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-          <meta name="theme-color" content="#317EFB" />
+          <meta name="application-name" content="Search Bible" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Search Bible" />
+          <meta name="description" content="Best way to Search Bible" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:title" content="Search Bible" />
+          <meta name="twitter:description" content="Best way to Search Bible" />
+          <meta name="twitter:creator" content="@Dave" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Search Bible" />
+          <meta property="og:description" content="Best way to Search Bible" />
+          <meta property="og:site_name" content="Search Bible" />
+          <meta property="og:url" content="https://search-bible.netlify.app" />
         </Head>
         <body>
           <Main />
