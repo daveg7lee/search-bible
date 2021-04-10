d1874e5ce20e6e00c2cc53a49469ac7fbdacc0f5
Dave <daveg7lee@gmail.com>
2021-04-10 19:54:02 +0900

    mistake

--- next.config.js
+++ next.config.js
@@ -2,15 +2,4 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
